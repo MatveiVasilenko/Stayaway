a8c7564f0e670272f5fb7ce2455c658bd2a6db93
Matvei Vasilenko <mat.vasilenkoo@gmail.com>
2018-11-27 16:19:49 +0200

    third

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,11 @@
+
+
+// $('#exampleModalCenter').on('shown.bs.modal', function () {
+//   $('#myInput').trigger('focus')
+// })
+
 new WOW().init();
+
 
 $(document).ready(function(){
   // $('.testimonial').slick({
@@ -23,10 +30,13 @@ $(document).ready(function(){
   // });
 
 $('.team').slick({
+
   slidesToShow: 4,
   slidesToScroll: 1,
   infinite: true,
   arrows: true,
+  prevArrow: '<svg class="arrow arrow__left tes" width="29px" height="56px"><use xlink:href="#arrowLeft"></use></svg>',
+  nextArrow: '<svg class="arrow arrow__right tes" width="29px" height="56px"><use xlink:href="#arrowRight"></use></svg>',
   responsive: [
     {
       breakpoint: 1024,
@@ -34,32 +44,29 @@ $('.team').slick({
         slidesToShow: 3,
         slidesToScroll: 3,
         infinite: true,
+        arrows:false
       }
     },
     {
       breakpoint: 600,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 2
+        slidesToScroll: 2,
+        arrows:false
+
       }
     },
     {
       breakpoint: 480,
       settings: {
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        arrows:false
+        
       }
     }
 
   ]
-  // prevArrow: '<img class="slick-prev" src="img/arrowLeft.png" alt="">',
- 	// nextArrow: '<img class="slick-next" src="img/arrowRight.png" alt="">'
-
-
 });
 
-
-// $('.carousel').carousel({
-//   interval: 10000
-// });
 });
